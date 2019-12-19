bd8878c7c50de85143d82fffbc11b0a59197f6a1
ngaivoronski <ngaivoronski@gmail.com>
2019-12-19 09:11:23 -0500

    mvp

---
+++ users/user-model.js
@@ -0,0 +1,34 @@
+const db = require('../data/dbConfig');
+
+module.exports={
+    getUsers,
+    getUserById,
+    getUserBy,
+    registerUser,
+}
+
+function getUsers() {
+    return db("users").select("id", "username", "department");
+}
+
+function getUserById(id) {
+    return db("users")
+        .select("id", "username", "department")
+        .where({ id })
+        .first();
+}
+
+function getUserBy(filter) {
+    return db("users")
+        .select("id", "username", "password", "department") // make sure to return the password
+        .where(filter);
+}
+
+function registerUser(user) {
+    return db("users")
+    .insert(user, "id")
+    .then(ids => {
+        const [id] = ids;
+        return getUserById(id);
+    });
+}
---
+++ users/user-router.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const Users = require('./user-model.js');
+const restricted = require('../auth/restricted-middleware.js');
+
+const role = 'admin';
+
+router.get('/', restricted, checkRole(role), (req, res) => {
+    Users.getUsers()
+        .then(users => {
+        res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+function checkRole(role) {
+    return function(req, res, next) {
+        if(req.token && role === req.token.role) {
+        next();
+        } else {
+        res.status(403).json({ message: `invalid role, you must be a ${role} to see all of the users.` })
+        }
+    }
+}
+
+module.exports = router;
